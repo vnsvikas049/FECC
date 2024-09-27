8e6adb457a9a6602281b282157fa3c1ea7dc0eed
vikas <vikasmishra049@yahoo.com>
2024-09-27 23:48:30 +0530

    Prob - 7

---
+++ JS/day 8/Problem1.js
@@ -0,0 +1,26 @@
+function Animal() {
+    this.type="animail";    
+}
+
+Animal.prototype.sound=function() {
+ return `Animal  sound.`
+
+}
+let a1=new Animal();
+console.log(a1.sound())
+
+
+function Dog() {
+  Animal.call(this) 
+    this.type='dog';
+}
+
+Dog.prototype.sound=function () {
+    return `Bark`;
+}
+
+Object.setPrototypeOf(Dog.prototype,Animal.prototype)
+
+let d1=new Dog();
+console.log(d1.sound());
+console.log(d1);
